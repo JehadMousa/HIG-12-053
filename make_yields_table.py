b23caf12f3eaeea7d5412fdc6c76ec2d352c089d
Evan Friis <ekfriis@gmail.com>
2013-01-22 04:51:12 -0600

    cleanup and PEP8

--- make_yields_table.py
+++ make_yields_table.py
@@ -1,12 +1,11 @@
 '''
 
-Build yields for HIG-12-032 results table using data card.
+Build yields for HIG-12-053 results table using data card.
 
 
 '''
 
 from DataCard import DataCard
-import pprint
 import string
 import sigfigs
 
@@ -15,23 +14,19 @@ megacard = DataCard('megacard_125.txt')
 
 # Store yields for various processes
 yields = {
-    'llt' : {},
-    'ltt' : {},
-    'zh' : {},
-    # tau_h tau_h
-    'boost' : {},
-    'vbf' : {},
+    'llt': {},
+    'ltt': {},
+    'zh': {},
 }
 
 # Get LLT yields
-llt_exclude = None
-#llt_exclude = '*eet*'
-yields['llt']['wz'] = megacard.get_rate('llt*', 'wz', excludebin=llt_exclude)
-yields['llt']['zz'] = megacard.get_rate('llt*', 'zz', excludebin=llt_exclude)
-yields['llt']['fakes'] = megacard.get_rate('llt*', 'fakes', excludebin=llt_exclude)
-yields['llt']['VH'] = megacard.get_rate('llt*', 'WH', excludebin=llt_exclude)
-yields['llt']['VHww'] = megacard.get_rate('llt*', 'WH_hww', excludebin=llt_exclude)
-yields['llt']['total'] = yields['llt']['wz'] + yields['llt']['zz'] + yields['llt']['fakes']
+yields['llt']['wz'] = megacard.get_rate('llt*', 'wz')
+yields['llt']['zz'] = megacard.get_rate('llt*', 'zz')
+yields['llt']['fakes'] = megacard.get_rate('llt*', 'fakes')
+yields['llt']['VH'] = megacard.get_rate('llt*', 'WH')
+yields['llt']['VHww'] = megacard.get_rate('llt*', 'WH_hww')
+yields['llt']['total'] = (
+    yields['llt']['wz'] + yields['llt']['zz'] + yields['llt']['fakes'])
 yields['llt']['obs'] = megacard.get_obs('llt*')
 
 #yields['ltt']['wz'] = megacard.get_rate('ltt*', 'wz')
@@ -39,7 +34,8 @@ yields['llt']['obs'] = megacard.get_obs('llt*')
 #yields['ltt']['fakes'] = megacard.get_rate('ltt*', 'fakes')
 #yields['ltt']['VH'] = megacard.get_rate('ltt*', 'VH')
 #yields['ltt']['VHww'] = 0
-#yields['ltt']['total'] = yields['ltt']['wz'] + yields['ltt']['zz'] + yields['ltt']['fakes']
+#yields['ltt']['total'] = (
+#  yields['ltt']['wz'] + yields['ltt']['zz'] + yields['ltt']['fakes'])
 
 yields['zh']['zz'] = megacard.get_rate('ZH*', 'ZZ')
 yields['zh']['fakes'] = megacard.get_rate('ZH*', 'Zjets')
@@ -48,8 +44,6 @@ yields['zh']['VHww'] = megacard.get_rate('ZH*', 'ZH_hww')
 yields['zh']['total'] = yields['zh']['zz'] + yields['zh']['fakes']
 yields['zh']['obs'] = megacard.get_obs('ZH*')
 
-#yields['zh']['obs'] = 0
-#yields['llt']['obs'] = 0
 
 def render(the_yield):
     if isinstance(the_yield, int):
