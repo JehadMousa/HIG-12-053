272b023a63949ae883a7c5dc950a4cadf20ff769
Evan Friis <ekfriis@gmail.com>
2013-01-23 08:42:45 -0600

    Improve/fix bugs in pud puller

--- do_pull.sh
+++ do_pull.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Run the ML fit and compute all in the information for the pulls.
 
@@ -8,14 +8,14 @@ combineCards.py *.txt > everything.txt
 
 for card in `ls *.txt`
 do
-  echo $card
+  echo "ML fitting $card in $PWD"
   name=`echo $card | sed "s|.txt||"`
-  echo $name
-  combine -M MaxLikelihoodFit --minimizerStrategy=2 --robustFit=1 --minimizerStrategyForMinos=2 $card -m 140 &> $name.combine.txt
-  #combine -M MaxLikelihoodFit $card -m 140 &> $name.combine.txt
+  combine -M MaxLikelihoodFit --minimizerStrategy=2 --robustFit=1 --minimizerStrategyForMinos=2 $card -m 125 &> $name.combine.txt
+  echo "Generating ML fit reports"
   python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py mlfit.root -f html > $name.pulls.html
   python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py mlfit.root -a -f html > $name.pulls.all.html
-  python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py mlfit.root -g $name.pulls.png > $name.pulls.txt
+  # requires 1.6 of http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/HiggsAnalysis/CombinedLimit/test/diffNuisances.py?view=log&pathrev=MAIN
+  #python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py mlfit.root -g $name.pulls.png > $name.pulls.txt
   mv mlfit.root $card.fit.root
 done
 
