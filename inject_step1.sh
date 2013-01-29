17bfd234228c90a3b020792c2defcaeebd94df6c
Evan Friis <ekfriis@gmail.com>
2013-01-29 09:37:15 -0600

    Skip 110

--- inject_step1.sh
+++ inject_step1.sh
@@ -7,5 +7,7 @@ find limits -type d  -exec fs setacl -dir '{}' -acl condor-hosts rlidkw \;
 for type in llt cmb ltt 4l;
 do
   echo "Submitting $type"
-  submit.py --injected --condor --toys 500 --bunch-masses 10 limits/$type/*
+  echo "FIXME I'm skipping 110"
+  # For now, filter out 110 GeV, since it isn't in the all hadronic cards.
+  ls limits/$type/* -d | grep -v 110 | xargs submit.py --injected --condor --toys 500 --bunch-masses 10 
 done
