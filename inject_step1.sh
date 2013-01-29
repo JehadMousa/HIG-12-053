a095c6bf84532dfce395614cb1f7ae9a34c906b8
Evan Friis <ekfriis@gmail.com>
2013-01-29 04:02:04 -0600

    Make sure the limit directories are writable by condor

--- inject_step1.sh
+++ inject_step1.sh
@@ -2,6 +2,8 @@
 
 # Submit 500 signal injection toy jobs for each limit type.
 
+find limits -type d  -exec fs setacl -dir '{}' -acl condor-hosts rlidkw \;
+
 for type in llt cmb ltt 4l;
 do
   echo "Submitting $type"
