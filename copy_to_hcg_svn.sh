b2b0d772ac6065d87081210d3733b54b671c70b2
Evan Friis <ekfriis@gmail.com>
2013-02-19 08:18:59 -0600

    remove hack

--- copy_to_hcg_svn.sh
+++ copy_to_hcg_svn.sh
@@ -10,5 +10,5 @@ echo "Copying 2nd Higgs search cards"
 cat searches_masses.txt | xargs -n 1 -I {} cp -r -v limits_2ndhiggs/vhtt/{} hcg/searches/vhtt/
 cp -r -v limits_2ndhiggs/vhtt/common hcg/searches/vhtt/
 
-echo "FIXME"
-perl -pi -e 's/CMS_vhtt_.*_shape_WH.*_bin.*//' hcg/*/vhtt/*/vhtt_2*txt
+#echo "FIXME"
+#perl -pi -e 's/CMS_vhtt_.*_shape_WH.*_bin.*//' hcg/*/vhtt/*/vhtt_2*txt
