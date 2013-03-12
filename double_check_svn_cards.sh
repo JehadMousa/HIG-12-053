3c0978fd329bdffc1914295bebe186b92112275a
Evan Friis <ekfriis@gmail.com>
2013-03-12 12:54:34 -0500

    switch order

--- double_check_svn_cards.sh
+++ double_check_svn_cards.sh
@@ -5,6 +5,17 @@ set -e
 # text2workspace.py on each sample.
 
 while read mass; do
+  for card in `ls hcg/searches/vhtt/$mass/*.txt`;
+  do
+    echo $card
+    text2workspace.py -m $mass $card \
+      -P HiggsAnalysis.CombinedLimit.PhysicsModel:strictSMLikeHiggs \
+      -o tmp.root 
+    #| grep -v "Warning: decay string"
+  done
+done < searches_masses.txt
+
+while read mass; do
   for card in `ls hcg/couplings/vhtt/$mass/*.txt`;
   do
     echo $card
@@ -15,15 +26,5 @@ while read mass; do
   done
 done < coupling_masses.txt
 
-while read mass; do
-  for card in `ls hcg/searches/vhtt/$mass/*.txt`;
-  do
-    echo $card
-    text2workspace.py -m $mass $card \
-      -P HiggsAnalysis.CombinedLimit.PhysicsModel:strictSMLikeHiggs \
-      -o tmp.root 
-    #| grep -v "Warning: decay string"
-  done
-done < searches_masses.txt
 
 echo "Everything okay!"
