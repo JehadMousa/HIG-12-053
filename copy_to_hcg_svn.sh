9313e84b19a228553a310a327c4d1385547ac91a
Evan Friis <ekfriis@gmail.com>
2013-03-12 12:53:42 -0500

    workaround 120 named 12 bug by making symlinks

--- copy_to_hcg_svn.sh
+++ copy_to_hcg_svn.sh
@@ -4,11 +4,11 @@
 
 echo "Copying coupling cards"
 cat coupling_masses.txt | xargs -n 1 -I {} cp -r -v limits/vhtt/{} hcg/couplings/vhtt/
-cp -r -v limits/vhtt/common hcg/couplings/vhtt/
+cp -L -r -v limits/vhtt/common hcg/couplings/vhtt/
 
 echo "Copying 2nd Higgs search cards"
-cat searches_masses.txt | xargs -n 1 -I {} cp -r -v limits_2ndhiggs/vhtt/{} hcg/searches/vhtt/
-cp -r -v limits_2ndhiggs/vhtt/common hcg/searches/vhtt/
+cat searches_masses.txt | xargs -n 1 -I {} cp -L -r -v limits_2ndhiggs/vhtt/{} hcg/searches/vhtt/
+cp -L -r -v limits_2ndhiggs/vhtt/common hcg/searches/vhtt/
 
 #echo "FIXME"
-#perl -pi -e 's/CMS_vhtt_.*_shape_WH.*_bin.*//' hcg/*/vhtt/*/vhtt_2*txt
+perl -pi -e 's/CMS_vhtt_.*_shape_WH.*_bin.*//' hcg/*/vhtt/*/vhtt_2*txt
